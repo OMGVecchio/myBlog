e8ad44c088495d54f964a3bea63547e5010dae16
vecchio <weichao@zbj.com>
2017-12-13 20:21:54 +0800

    go home

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,17 @@
 'use strict'
 
 import * as React from "react"
-import * as ReactDOM from "react-dom"
-import * as ReactRouter from 'react-router'
+import { render } from "react-dom"
+import { Router, Route, MemoryRouter } from 'react-router'
+// history 从 react-router 库中脱离
+import { createBrowserHistory } from 'history'
 
 import { Hello } from "./components/Hello"
 
-ReactDOM.render(
-    <ReactRouter.Router history={ ReactRouter.hashHistory} >
-        <ReactRouter.Route path="/" component={ Hello } />
-    </ReactRouter.Router>
+render(
+    <Router history={ createBrowserHistory() } >
+        <Route path="/" component={ Hello } />
+    </Router>
     ,
     document.getElementById("app")
 )
