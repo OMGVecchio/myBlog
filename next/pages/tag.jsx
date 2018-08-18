8d8817413e2ea287143ba030c9f8651b01fa1c67
vecchio <weichao@zbj.com>
2018-08-18 23:51:27 +0800

    go to modify tag-sign

--- next/pages/tag.jsx
+++ next/pages/tag.jsx
@@ -8,6 +8,8 @@ import { Row, Col } from 'antd'
 import Layout from 'components/layout'
 import TagCard from 'components/card/tag'
 
+import Moment from 'utils/moment'
+
 class Tag extends PureComponent {
   static defaultProps = {
     articleList: []
@@ -18,9 +20,59 @@ class Tag extends PureComponent {
   }
   showCardList = (cardData = []) => {
     let cardDataCache = null
+    const signTag = { year: '', month: '' }
     const cardList = []
-    cardData.forEach((item) => {
-      if (!cardDataCache) {
+    const total = cardData.length
+    cardData.forEach((item, index) => {
+      let hasSameSign = true
+      const { createTime } = item
+      const date = new Moment(createTime)
+      const year = date.getYear()
+      const month = date.getMonth()
+      if (signTag.month !== month) {
+        signTag.month = month
+        hasSameSign = false
+      }
+      if (signTag.year !== year) {
+        signTag.year = year
+        hasSameSign = false
+      }
+      if (!hasSameSign) {
+        if (cardDataCache) {
+          cardList.push((
+            <Row gutter={20} key={cardDataCache.id}>
+              <Col span={12}>
+                <TagCard {...cardDataCache} />
+              </Col>
+            </Row>
+          ))
+        }
+        cardList.push((
+          <h3 id={`${signTag.year}-${signTag.month}`}>
+            {signTag.year}年{signTag.month}月
+          </h3>
+        ))
+        if (total === index + 1) {
+          cardList.push((
+            <Row gutter={20} key={item.id}>
+              <Col span={12}>
+                <TagCard {...item} />
+              </Col>
+            </Row>
+          ))
+        } else {
+          cardDataCache = item
+        }
+      } else if (!cardDataCache) {
+        if (total === index + 1) {
+          cardList.push((
+            <Row gutter={20} key={item.id}>
+              <Col span={12}>
+                <TagCard {...item} />
+              </Col>
+            </Row>
+          ))
+        }
         cardDataCache = item
       } else {
         cardList.push((
