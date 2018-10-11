8bd6c90357fe10267f413317da0d7eb0d48c2cc5
vecchio <weichao@zbj.com>
2018-10-11 18:22:06 +0800

    want to draw itachi

--- next/components/layout/index.jsx
+++ next/components/layout/index.jsx
@@ -10,6 +10,7 @@ import types from '#/action/common'
 import Header from '~/common/header'
 import Footer from '~/common/footer'
 import Menu from '~/common/aside'
+import Pet from '~/common/pet'
 import BackTop from '~/base/backtop'
 import LinearProgress from '~/base/linearprogress'
 
@@ -113,6 +114,7 @@ class Layout extends PureComponent {
         </Head>
         <LinearProgress visible={this.props.globalProgress} className="global-router-progress" />
         <Menu />
+        <Pet />
         <div className={classNames('main-wrap', { 'menu-has-close': !asideIsOpen })}>
           <Header title={title} isLongScroll={isLongScroll} />
           <BackTop show={isLongScroll} />
